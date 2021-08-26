8d88d01d36fef6dfa3dca12a2c470d507fc51791
Rokas <rookas.rudzenskas@gmail.com>
2021-08-26 21:38:38 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Rookas Airbnb</title>
         <link rel="icon" href="/favicon.png" />
