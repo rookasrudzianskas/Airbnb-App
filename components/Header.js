9f695534a7ee0d67ad6387470faef0277b0202fd
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 12:47:41 +0300

    Roaks

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import Image from "next/image";
 import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon } from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const Header = () => {
 
