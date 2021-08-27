c3cde9cd5c694790ef9245b407d42313508503cc
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 10:04:17 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -17,10 +17,10 @@ const Header = () => {
                 />
             </div>
 
-            <div className="flex">
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
             {/*    middle   */}
-                <input type="text" placeholder={"Start your search"}/>
-                <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+                <input type="text" className="flex-grow pl-5 bg-transparent outline-none" placeholder={"Start your search"}/>
+                <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
             <div>
