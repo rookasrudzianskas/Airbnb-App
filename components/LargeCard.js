19e12ccef508c11d3a1f03b5abd317dd73054f8f
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 11:27:27 +0300

    Rokas

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -3,8 +3,22 @@ import Image from "next/image";
 
 const LargeCard = ({img, title, description, buttonText}) => {
     return (
-        <section>
+        <section className="relative py-16 cursor-pointer">
+            <div className="relative h-96 min-w-[300px]">
+                <Image
+                    src={img}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-2xl"
+                />
+            </div>
 
+            <div className="absolute top-32 left-12">
+                <h3 className="text-4xl mb-3 w-64">{title}</h3>
+                <p className="">{description}</p>
+
+                <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 hover:bg-transparent hover:text-gray-900 border-2 border-gray-900 transition duration-150 ease-out">{buttonText}</button>
+            </div>
         </section>
     );
 };
