68f14b793e9a08bc40b059752a9925bb3400b1fb
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 09:25:33 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+            <h1>Header</h1>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-    {/*    Header   */}
+        <div>
+            <Header />
+        </div>
     {/*    Banner   */}
     </div>
   )
