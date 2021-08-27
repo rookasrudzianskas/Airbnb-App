29b4e5e01252b95d89e1cf0efb165811d9ecae26
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 13:49:36 +0300

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -18,6 +18,10 @@ const Search = () => {
                     <p className="text-xs">300+ stays for 5 number of guests</p>
 
                     <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+
+                    <div className="hidden lg:inline-flex">
+                        <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Cancellation Flexibility</p>
+                    </div>
                 </section>
             </main>
 
