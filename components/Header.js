cde09bc5629d2be5084fac5aa806866d01c2523d
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 09:46:55 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
 
 
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
