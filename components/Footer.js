2258d00a95fb54afe81ca48b54382f92d7d95664
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 11:43:02 +0300

    Rokas

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100">
                 <div className="space-y-4 text-sm text-gray-800">
                     <h5 className="font-bold">ABOUT</h5>
                     <p>How Airbnb works</p>
