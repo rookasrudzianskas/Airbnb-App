cf5ff3a852b529fc082850713327afefa3bffc60
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 09:56:08 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Image from "next/image";
+import { SearchIcon } from '@heroicons/react/solid';
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 
 
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -16,9 +17,10 @@ const Header = () => {
                 />
             </div>
 
-            <div>
+            <div className="flex">
             {/*    middle   */}
-
+                <input type="text" placeholder={"Start your search"}/>
+                <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
             </div>
 
             <div>
