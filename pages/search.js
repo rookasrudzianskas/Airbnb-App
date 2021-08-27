b523b3153a59d12c2bc2f9ef32497751f811f812
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 13:40:49 +0300

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Head from "next/head";
 
 const Search = () => {
     return (
         <div>
-            
+            <Head>
+                <title>Airbnb search results</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
         </div>
     );
 };
