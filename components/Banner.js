c8ecc9099867dbbd9a5594b5a9e6cdf745287d5a
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 10:27:03 +0300

    Rokas

--- components/Banner.js
+++ components/Banner.js
@@ -8,6 +8,11 @@ const Banner = () => {
                 layout="fill"
                 objectFit="cover"
             />
+
+            <div className="absolute top-1/2 w-full text-center">
+                <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+                <button className="text-purple-500 bg-white px-10 py-4  shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
+            </div>
         </div>
     );
 };
