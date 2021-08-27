1dcc90ec0b704d4ea73c2fd040f8471c28b772b4
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 11:18:33 +0300

    Rokas

---
+++ components/LargeCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LargeCard = ({img, title, description, buttonText}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LargeCard;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
 
 export default function Home({exploreData, cardsData}) {
   return (
@@ -32,12 +33,20 @@ export default function Home({exploreData, cardsData}) {
             <section className="">
                 <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
-                <div className="flex space-x-4 overflow-scroll ">
+                <div className="flex space-x-4 overflow-scroll scrollbar-hide p-3 -ml-3">
                     {cardsData?.map(({img, title}) => (
                         <MediumCard key={img} img={img} title={title}/>
                     ))}
                 </div>
             </section>
+
+            <LargeCard
+                img="https://links.papareact.com/4cj"
+                title="The Greatest Outdoors"
+                description="Wishlists curated by Airbnb"
+                buttonText="Get Inspired"
+            />
+
         </main>
     </div>
   )
