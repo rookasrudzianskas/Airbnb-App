9a66cca43b48f4a91058109a5684a692c3af4ad4
Rokas <rookas.rudzenskas@gmail.com>
2021-08-27 11:09:19 +0300

    Rokas

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Image from "next/image";
 
 const MediumCard = ({img, title}) => {
     return (
-        <div>
+        <div className="">
+            <div className="relative h-80 w-80">
+                <Image
+                    src={img}
+                    layout="fill"
+                />
+
+            </div>
+
+            <h3>{title}</h3>
 
         </div>
     );
